f180bd767986ae8a8c52634bb6b1dbd771cb2721
gLuca88 <146572496+gLuca88@users.noreply.github.com>
2024-03-04 16:03:04 +0100

    eliminazione codice in più

--- src/main/java/Model/Appuntamento.java
+++ src/main/java/Model/Appuntamento.java
@@ -4,6 +4,7 @@ import java.sql.Date;
 import java.sql.Time;
 
 public class Appuntamento {
+	
 	private int id;
 	private Date dataAppuntamento;
 	private String descrizione;
@@ -81,5 +82,5 @@ public class Appuntamento {
 	public void setStato(String stato) {
 		this.stato = stato;
 	}
-
+    
 }// classe
